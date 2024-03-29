8adcf1532ccc435feb017ee4ab583277adc084f8
lellithan <faulhaberellie1@gmail.com>
2024-03-28 20:38:25 -0700

    dea

--- VisualStudioHTML/VisualStudioHTML/wwwroot/js/site.js
+++ VisualStudioHTML/VisualStudioHTML/wwwroot/js/site.js
@@ -9,3 +9,20 @@ function newColor() {
 function oldColor() {
     document.getElementById("hello_world").style.color = "darkolivegreen";
 }
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
